2e4d8c2fbc1c88c98db857698b7bc07fe91b0f7f
Denes Boros <boros.denes@gmail.com>
2018-05-12 12:39:14 +0200

    Customized preprocessing function to fit CNN

--- environment_utils.py
+++ environment_utils.py
@@ -1,12 +1,30 @@
 import numpy as np
+from PIL import Image
 
-def prepro(I, flatten=True):
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+
+
+def prepro(I, flatten=True, color='bin', downsample='simple'):
     """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
     I = I[35:195]  # crop
-    I = I[::2, ::2, 0]  # downsample by factor of 2
-    I[I == 144] = 0  # erase background (background type 1)
-    I[I == 109] = 0  # erase background (background type 2)
-    I[I != 0] = 1  # everything else (paddles, ball) just set to 1
+
+    if downsample == 'simple':
+        I = I[::2, ::2, 0]  # downsample by factor of 2
+
+    if (downsample == 'pil') & (color == 'gray'):
+        I = np.array(Image.fromarray(I).convert(mode='L').resize((80, 80)))
+
+    elif downsample == 'cv2':
+        raise NotImplementedError
+
+    if color == 'bin':
+        I[I == 144] = 0  # erase background (background type 1)
+        I[I == 109] = 0  # erase background (background type 2)
+        I[I != 0] = 1  # everything else (paddles, ball) just set to 1
     if flatten:
         return I.astype(np.float).ravel()
     else:
