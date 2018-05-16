a9d213ea899497b3b65658e02c52af2f851ea839
Denes Boros <boros.denes@gmail.com>
2018-05-16 13:17:27 +0200

    major interface update: identifier defines almost everything

--- pong.py
+++ pong.py
@@ -21,34 +21,62 @@ from environment_utils import prepro, discount_rewards
 # -- ARGUMENT PARSING ---------------------------------------
 # -----------------------------------------------------------
 parser = argparse.ArgumentParser()
+parser.add_argument('-id', '--identifier', type=str, required=True,
+                    help='''
+                    You can define loss type, optimizer and etc here.
+                    Format: networktype__network_details__preprotype__hyperparams
+                    
+                    cnn__c(1.3.2)_c(2.3.1)_f(6400.3)__prepro(gray)__b(6400)_l(0.001)_d(0.99)
+                    cnn__c(3.3.10)_c(10.3.1)_f(6400.3)__prepro(None)__b(6400)_l(0.001)_d(0.99)
+                    mlp__f(6400.256)_f(256.3)__prepro(bin)__b(6400)_l(0.001)_d(0.99)''')
+
 parser.add_argument('--environment', type=str, required=True,
                     help='Pong-v0')
-parser.add_argument('--network_type', type=str, required=True,
-                    help='[mlp, cnn, mlp_lstm, cnn_lstm]')
-parser.add_argument('--learning_rate', type=float, default=0.001)
-parser.add_argument('--batch_size', type=int, required=True,
-                    help='6400')
 parser.add_argument('--load', type=str, default=None,
                     help='Path to model folder to load. Must have same architecture as defined by args.')
 parser.add_argument('--checkpoint_every_n_episodes', type=int, default=10)
-parser.add_argument('--discount_factor', type=float, default=0.99)
 parser.add_argument('--render', type=int, help='How often to render during training.')
 parser.add_argument('--summarize_every_n_episodes', type=int, default=1)
-parser.add_argument('-id', '--identifier', type=str, required=True,
-                    help='You can define loss type, optimizer and etc here.'
-                         'e.g. cnn__c132_c231_f6400-3__b6400_l001_d99')
-parser.add_argument('--prepro', type=str, default='gray',
-                    help='[gray, bin]')
 parser.add_argument('--clean', action='store_true')
-parser.add_argument('--show_observation', action='store_true')
 args = parser.parse_args()
 
+if args.environment not in ['Pong-v0']:
+    raise NotImplementedError
+
+# -- VARIABLE INITIALIZER BASED ON GIVEN ID -----------------
 # -----------------------------------------------------------
-action_dictionary = {
-    0:0,
-    1:2,
-    2:3
-}
+given_id = args.identifier
+network_type, network_details, preprocess_type, hyperparameters = given_id.split('__')
+
+network_details = network_details.split('_')
+cnn_layers = [element for element in network_details if 'c' in element]
+dense_layers = [element for element in network_details if 'f' in element]
+lstm_layers = [element for element in network_details if 'l' in element]
+
+if len(lstm_layers) != 0:
+    raise NotImplementedError
+
+
+def unwrap(string_with_parathesis):
+    return string_with_parathesis[string_with_parathesis.find('(')+1:string_with_parathesis.find(')')]
+
+
+preprocess_type = unwrap(preprocess_type)
+if preprocess_type == 'None':
+    preprocess_type = None
+
+batch_size, learning_rate, discount_rate = map(unwrap, hyperparameters.split('_'))
+batch_size = int(batch_size)
+learning_rate = float(learning_rate)
+discount_rate = float(discount_rate)
+
+# -----------------------------------------------------------
+if args.environment == 'Pong-v0':
+    action_dictionary = {
+        0: 0,
+        1: 2,
+        2: 3
+    }
 
 if args.clean:
     try:
@@ -87,7 +115,7 @@ logger.debug('''Setting up training ...'
                 Training id : {}
                 
                 )'''
-             .format(args.environment, args.batch_size, args.network_type, args.load, args.identifier))
+             .format(args.environment, batch_size, network_type, args.load, args.identifier))
 # -----------------------------------------------------------
 
 # -- INITIALIZING STEPS -------------------------------------
@@ -97,15 +125,23 @@ config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 logger.debug('Setting up training placeholders, variables and graph ...')
 
-if args.network_type == 'mlp':
+if network_type == 'mlp':
     observations = tf.placeholder(tf.float32,
                                   [None, 80*80],
                                   name='observations')
-elif args.network_type == 'cnn':
-    observations = tf.placeholder(tf.float32,
-                              [None, 80, 80],
-                              name='observations')
-    observation_images = tf.reshape(observations, [-1, 80, 80, 1])
+elif network_type == 'cnn':
+    if preprocess_type == 'gray':
+        observations = tf.placeholder(tf.float32,
+                                      [None, None, None, 1],
+                                      name='observations')
+    elif preprocess_type == None:
+        observations = tf.placeholder(tf.float32,
+                                      [None, None, None, 3],
+                                      name='observations')
+    else:
+        raise NotImplementedError
+
+    # observation_images = tf.reshape(observations, [-1, 80, 80, 1])
 
 
 actions = tf.placeholder(dtype=tf.int32, shape=[None], name='actions')
@@ -120,50 +156,72 @@ tf.summary.scalar('mean_game_length', mean_game_length)
 global_step = tf.Variable(0, name='global_step', trainable=False)
 # -- TRAINING SETUP -----------------------------------------
 # -----------------------------------------------------------
-if args.network_type == 'mlp':
+layers = []
+if network_type == 'mlp':
+    for idx, layer in enumerate(dense_layers):
+        input_len, output_len = map(int, unwrap(layer).split('.'))
+        if idx == 0:
+            layers.append(
+                tf.layers.dense(inputs=observations,
+                                uints=output_len,
+                                activation=tf.nn.relu)
+            )
+        else:
+            layers.append(
+                tf.layers.dense(inputs=layers[idx-1],
+                                uints=output_len,
+                                activation=tf.nn.relu)
+            )
 
-    Y = tf.layers.dense(inputs=observations,
-                        units=256,
-                        activation=tf.nn.relu)
+elif network_type == 'cnn':
+    for idx, layer in enumerate(cnn_layers):
+        input_channels, kernel_size, output_channels = map(int, unwrap(layer).split('.'))
+        if idx == 0:
+            layers.append(
+                tf.layers.conv2d(inputs=observations,
+                                 filters=output_channels,
+                                 kernel_size=[kernel_size, kernel_size],
+                                 padding='same',
+                                 activation=tf.nn.relu)
+            )
+        else:
+            layers.append(
+                tf.layers.conv2d(inputs=layers[idx-1],
+                                 filters=output_channels,
+                                 kernel_size=[kernel_size, kernel_size],
+                                 padding='same',
+                                 activation=tf.nn.relu)
+            )
 
-    Ylogits = tf.layers.dense(inputs=Y,
-                              units=3)
+    if preprocess_type == 'gray':
+        layers.append(
+            layers.reshape(layers[-1], [-1, 80*80])
+        )
+    elif preprocess_type == None:
+        layers.append(
+            tf.reshape(layers[-1], [-1, 210*160])
+        )
 
-
-if args.network_type == 'cnn':
-    conv_1 = tf.layers.conv2d(inputs=observation_images,
-                              filters=2,
-                              kernel_size=[3, 3],
-                              padding='same',
-                              activation=tf.nn.relu)
-
-    conv_2 = tf.layers.conv2d(inputs=conv_1,
-                              filters=1,
-                              kernel_size=[3, 3],
-                              padding='same',
-                              activation=tf.nn.relu)
-
-    flattened = tf.reshape(conv_2, [-1, 80*80])
-
-    # hidden = tf.layers.dense(inputs=flattened,
-    #                          units=256,
-    #                          activation=tf.nn.relu)
-
-    Ylogits = tf.layers.dense(inputs=flattened,
-                              units=3)
+    for layer in dense_layers:
+        input_len, output_len = map(int, unwrap(layer).split('.'))
+        layers.append(
+            tf.layers.dense(inputs=layers[-1],
+                            units=output_len,
+                            activation=tf.nn.relu)
+        )
 
 # TODO: LSTM with logit output of size 3
 
-sample_op = tf.multinomial(logits=tf.reshape(Ylogits, shape=(1, 3)), num_samples=1)
+sample_op = tf.multinomial(logits=tf.reshape(layers[-1], shape=(1, 3)), num_samples=1)
 
 with tf.name_scope('cross_entropy'):
     cross_entropy = tf.losses.softmax_cross_entropy(onehot_labels=tf.one_hot(actions, 3),
-                                                    logits=Ylogits,
+                                                    logits=layers[-1],
                                                     weights=rewards)
     tf.summary.scalar('cross_entropy', cross_entropy)
 
 with tf.name_scope('training'):
-    optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
     train_op = optimizer.minimize(cross_entropy, global_step=global_step)
 
 
@@ -205,28 +263,34 @@ with sess:
         _actions = []
         _rewards = []
 
-        while len(_observations) < args.batch_size:
+        while len(_observations) < batch_size:
             before_counter = len(_observations)
             logger.debug('Collected {} of {} observations ({:.1f} %).'.format(before_counter,
-                                                                              args.batch_size,
-                                                                              float(before_counter) / args.batch_size * 100))
-            if args.prepro == 'gray':
+                                                                              batch_size,
+                                                                              float(before_counter) / batch_size * 100))
+            if preprocess_type == 'gray':
                 previous_pix = prepro(env.reset(), flatten=False, color='gray', downsample='pil')
-            elif args.prepro == 'bin':
+            elif preprocess_type == 'bin':
                 previous_pix = prepro(env.reset())
+            elif preprocess_type != None:
+                raise NotImplementedError
+            else:
+                previous_pix = env.reset()
 
             game_state, _, done, _ = env.step(env.action_space.sample())
             game_counter += 1
 
             while not done:
-                if args.prepro == 'gray':
+                if preprocess_type == 'gray':
                     current_pix = prepro(game_state, flatten=False, color='gray', downsample='pil')
-                elif args.prepro == 'bin':
+                elif preprocess_type == 'bin':
                     current_pix = prepro(game_state)
+                elif preprocess_type != None:
+                    raise NotImplementedError
+                else:
+                    current_pix = game_state
 
-                observation = current_pix - previous_pix
-                if args.show_observation:
-                    PIL.Image.fromarray(observation).show()
+                observation = current_pix
                 previous_pix = current_pix
 
                 if args.render:
@@ -246,7 +310,7 @@ with sess:
         logger.info('Played {} games. Mean reward is {:.2f}'
                     .format(game_counter, avg_rewards))
 
-        processed_rewards = discount_rewards(_rewards, args.discount_factor)
+        processed_rewards = discount_rewards(_rewards, discount_rate)
 
         processed_rewards -= np.mean(processed_rewards)
         processed_rewards /= np.std(processed_rewards)
